586d3f0a20e170625ad289212110fda6aa29b445
RichForever <mateuszmisiak1@gmail.com>
2024-10-22 10:22:33 +0200

    update styles

--- src/components/TodoWrapper.tsx
+++ src/components/TodoWrapper.tsx
@@ -9,7 +9,10 @@ const TodoWrapper = () => {
     const [todos, setTodos] = useState<ITodoItem[]>([]);
 
     return (
-        <Center p={8} backgroundColor="white" height="100vh" borderColor="gray.100" borderWidth="1px">
+        <Center p={{
+            base: 4,
+            xl: 8
+        }} backgroundColor="white" height="100vh" borderColor="gray.100" borderWidth="1px">
             <VStack maxW="500px" width="100%" gap={4} backgroundColor="gray.50" padding={4} borderRadius="xl">
                 <TodoForm setTodos={setTodos} todos={todos} />
                 <TodoList todos={todos} setTodos={setTodos} />
