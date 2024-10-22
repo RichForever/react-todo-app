9b5f516d40da7da4e11ae440153b73edc97b54a4
RichForever <mateuszmisiak1@gmail.com>
2024-10-22 10:18:04 +0200

    fix for clearing localStorage

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoWrapper from "./TodoWrapper";
 
 const Todo = () => {
+
+    useEffect(() => {
+        const colorMode = localStorage.getItem('chakra-ui-color-mode')
+        if(colorMode && colorMode !== 'light') {
+            localStorage.setItem("chakra-ui-color-mode", "light");
+        }
+    }, []);
     return (
         <div>
             <TodoWrapper />
